e6d4a4e81f5a4a9d117049cb83c0ecd6e869536e
kamalshanth163 <kamalshanthdeveloper163@gmail.com>
2020-03-16 09:58:37 +0530

    Initial commit

---
+++ SocialApp_API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using SocialApp_API.Models;
+
+namespace SocialApp_API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Value> Values { get; set; }
+    }
+}
