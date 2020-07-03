ee02f7524381eb47b14c80fff997ada2dc023820
kamalshanth163 <kamalshanthdeveloper163@gmail.com>
2020-07-03 18:00:18 +0530

    Added Like functionality

--- SocialApp_API/Controllers/UsersController.cs
+++ SocialApp_API/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using SocialApp_API.Data;
 using SocialApp_API.Dtos;
 using SocialApp_API.Helpers;
+using SocialApp_API.Models;
 
 namespace SocialApp_API.Controllers {
     [ServiceFilter (typeof (LogUserActivity))]
@@ -62,6 +63,35 @@ namespace SocialApp_API.Controllers {
                 return NoContent ();
 
             throw new Exception ($"Updating user {id} failed on save");
+        }
+
+        [HttpPost("{id}/like/{recipientId}")]
+        
+        public async Task<IActionResult> LikeUser(int id, int recipientId)
+        {
+            if(id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var like = await _repo.GetLike(id, recipientId);
+
+            if(like != null)
+                return BadRequest("You already like this user");
+            
+            if(await _repo.GetUser(recipientId) == null)
+                return NotFound();
+
+            like = new Like
+            {
+                LikerId = id,
+                LikeeId = recipientId
+            };
+
+            _repo.Add<Like>(like);
+
+            if(await _repo.SaveAll())
+                return Ok();
+
+            return BadRequest("Failed to like user");
         }
 
     }
